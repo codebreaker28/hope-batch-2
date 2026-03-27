1f54e9435ed81434c1cb44ff753bc4cd989e29a7
codebreaker28 <bananth871@gmail.com>
2026-03-27 14:28:01 +0530

    Implement robot bounded movement solution

---
+++ leetcode_1041.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        int x=0,y=0;
+        int dir=0;
+        for(char ch:instructions.toCharArray())
+        {
+            if(ch=='G')
+            {
+                if(dir==0) y++;
+                else if(dir==1) x++;
+                else if(dir==2) y--;
+                else if(dir==3) x--;
+            }
+            else if(ch=='L')
+            {
+                dir=(dir+3)%4;
+            }
+            else if(ch=='R') 
+            {
+                dir=(dir+1)%4;
+            }
+
+        }
+        return (x==0&&y==0 || dir!=0); 
+        
+    }
+}
