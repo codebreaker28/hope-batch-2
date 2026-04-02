51912a68afbb187fb85e93c12dc3e0d2517958e7
codebreaker28 <bananth871@gmail.com>
2026-04-02 14:34:21 +0530

    Add initial implementation of SecondMostFreq class

---
+++ SecondMostFreq.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class SecondMostFreq {
+    public static void main(String[] args) {
+        {
+            Scanner sc=new Scanner(System.in);
+
+            String str=sc.nextLine();
+            // int count=0;
+            int firstmax=0,secondmax=0;
+            char firstmaxchar=' ',secondmaxchar=' ';
+            for(int i=0;i<str.length();i++)
+            {
+                char ch=str.charAt(i);
+                int count=0;
+                
+            }
+
+        }
+    }
+    
+}
