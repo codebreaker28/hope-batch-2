16a046d90507c4537890ebb3505b283d9b95bd84
codebreaker28 <bananth871@gmail.com>
2026-04-05 15:48:49 +0530

    Create player.java

---
+++ video_streaming_system/player.java
@@ -0,0 +1,9 @@
+class Player {
+    public void play(Video video) {
+        System.out.println("Playing video: " + video.getTitle());
+    }
+
+    public void pause() {
+        System.out.println("Video paused");
+    }
+}
